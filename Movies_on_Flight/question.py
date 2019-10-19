cea4b01f3f04f6de2fd2ca3132d1f92427669f71
QL <qli@phone2action.com>
2019-10-19 11:17:05 -0400

    add answer for movies on flight

--- Movies_on_Flight/question.py
+++ Movies_on_Flight/question.py
@@ -9,6 +9,29 @@
 # movie_duration: [90, 85, 75, 60, 120, 150, 125]
 # d: 250
 
-# Output from aonecode.com
 # [90, 125]
 # 90min + 125min = 215 is the maximum number within 220 (250min - 30min)
+
+
+def selectMovies(movies, flight_duration):
+	movies.sort()
+	max_time = 0
+	target_time = flight_duration - 30
+	num_movies = len(movies)
+	result = []
+
+	#movies = [60, 75, 85, 90, 120, 125, 150]
+
+	for i in range(num_movies-1, 1, -1):
+		for j in range(num_movies-2, 0, -1):
+			time_sum = movies[i] + movies[j]
+			if time_sum <= target_time:
+				if time_sum > max_time:
+					max_time = time_sum
+					result = [movies[j], movies[i]]
+
+	return result
+
+
+print(selectMovies([90, 85, 75, 60, 120, 150, 125], 250)) # [90, 125]
+print(selectMovies([90, 85, 75, 60, 120, 150, 125], 300)) # [120, 150]
