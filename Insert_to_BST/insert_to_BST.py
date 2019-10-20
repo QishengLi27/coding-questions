d4f2afb5203981501401521476a1250effedef16
QL <qli@phone2action.com>
2019-10-20 03:53:35 -0400

    two other questions

---
+++ Insert_to_BST/insert_to_BST.py
@@ -0,0 +1,23 @@
+# Description
+# Given a binary search tree and a new tree node, insert the node into the tree. You should keep the tree still be a valid binary search tree.
+
+# Node:
+# class Node:
+#     def __init__(self, val):
+#         self.val = val
+#         self.left = None 
+#		  self.right = None
+
+def insertNode(root, node):
+        return insert(root, node)
+    
+def insert(root, node):
+    if root is None:
+        return node
+    
+    if node.val < root.val:
+        root.left = insert(root.left, node)
+    else:
+        root.right = insert(root.right, node)
+    
+    return root
