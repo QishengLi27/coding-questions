b811bde50bf0ac9b48a9ff936e22e860067df61c
QL <qli@phone2action.com>
2020-01-03 23:56:32 -0500

    leetcode 136

---
+++ Single_Number_leetcode_136/single_number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/single-number/
+
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+# Input: [4,1,2,1,2]
+# Output: 4
+
+# using XOR, a list of numns become a1^a2^a1^a3^a1, which is (a1^a1)^(a2^a2)^a3
+def singleNumber(self, nums: List[int]) -> int:
+	result = 0
+		for num in nums:
+			result ^=num
+	return result
