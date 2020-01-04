72d5c6b812c1695a76fbdf88eec935d59ab1c355
QL <qli@phone2action.com>
2020-01-04 00:26:46 -0500

    remove self

--- Single_Number_leetcode_136/single_number.py
+++ Single_Number_leetcode_136/single_number.py
@@ -5,7 +5,7 @@
 # Output: 4
 
 # using XOR, a list of numns become a1^a2^a1^a3^a1, which is (a1^a1)^(a2^a2)^a3
-def singleNumber(self, nums: List[int]) -> int:
+def singleNumber(nums: List[int]) -> int:
 	result = 0
 		for num in nums:
 			result ^=num
